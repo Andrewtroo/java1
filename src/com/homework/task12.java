e56787ff1eddd0c8dd1cc6cb57b0861be8095640
Andrewtroo <andrewtroo@gmail.com>
2022-06-23 22:34:28 +0300

    task12

--- src/com/homework/task12.java
+++ src/com/homework/task12.java
@@ -3,7 +3,6 @@ package com.homework;
 import java.util.Scanner;
 
 public class task12 {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Your question: ");
